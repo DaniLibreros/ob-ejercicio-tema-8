f219fefbe410a7e89e7b21474c90d202f199a56f
Dlibreros <libreros1226@gmail.com>
2022-05-23 10:56:18 -0500

    Ejercicio tema 8: Crear una objeto de tipo Persona

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        Persona daniela = new Persona ();
+        daniela.setName( "Daniela Libreros" );
+        daniela.setAge( 25 );
+        daniela.setPhoneNumber( 123456789 );
+
+        System.out.println( "Name:" + daniela.getName ());
+        System.out.println( "Age:" + daniela.getAge ());
+        System.out.println( "PhoneNumber:" + daniela.getPhoneNumber ());
+    }
+}
+
+
+
+
---
+++ src/Persona.java
@@ -0,0 +1,30 @@
+public class Persona {
+
+   private String name;
+   private int age;
+   private int phoneNumber;
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public int getAge() {
+      return age;
+   }
+
+   public void setAge(int age) {
+      this.age = age;
+   }
+
+   public int getPhoneNumber() {
+      return phoneNumber;
+   }
+
+   public void setPhoneNumber(int phoneNumber) {
+      this.phoneNumber = phoneNumber;
+   }
+}
